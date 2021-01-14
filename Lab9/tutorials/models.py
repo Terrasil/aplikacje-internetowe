17755b766689243702c6abffc8f8c1859177379d
Patryk Morawski <gormit7@gmail.com>
2021-01-14 19:23:58 +0100

    Pliki do laboratorium 9

---
+++ Lab9/tutorials/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+def upload_path(instance, filename):
+    return '/'.join(['phones',str(instance.title),filename])
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=1000,blank=False, default='')
+    price = models.FloatField(blank=False, default=0)
+    published = models.BooleanField(default=False)
+    upload = models.ImageField(blank=True, null=True, upload_to=upload_path)
---
+++ Lab9/tutorials/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url 
+from tutorials import views 
+from django.urls import path
+from .views import PostDetail
+urlpatterns = [ 
+    path('api/tutorials', views.PostView.as_view(), name='posts_list'),
+    path('api/tutorials/', views.SearchPostsView.as_view(), ),
+    path('api/tutorials/<int:pk>/', PostDetail.as_view()),
+]
